21f442158c88cfd18b9a06d414208fc0e7085f78
tal <tals@ybrantdigital.com>
2016-09-10 22:40:14 +0300

    development

--- wp-content/themes/fftheme/ffhome-page.php
+++ wp-content/themes/fftheme/ffhome-page.php
@@ -11,10 +11,11 @@
 
 get_header('ff');
 ?>
-<img id="midHomeImg" src="http://stage.findingfootage.com/wp-content/themes/fftheme/images/new/DesktopSlicing/keywords-bg.jpg" alt="">
 
 <div id="txthome2" class="home_content">
-	<?php 
+    <img id="midHomeImg" src="http://stage.findingfootage.com/wp-content/themes/fftheme/images/new/DesktopSlicing/keywords-bg.jpg" alt="">
+
+    <?php
         if (have_posts()) : while (have_posts()) : the_post(); 
             endwhile;
         endif; 
@@ -22,99 +23,9 @@ get_header('ff');
     
     
     <?php  the_content(); ?>
-	
+
     <iframe style="border: none; background: #fff; overflow: hidden; width: 292px; height: 258px;" src="//www.facebook.com/plugins/likebox.php?href=http%3A%2F%2Fwww.facebook.com%2FFindingFootage&amp;width=292&amp;height=258&amp;show_faces=true&amp;colorscheme=light&amp;stream=false&amp;border_color&amp;header=false" frameborder="0" scrolling="no" width="320" height="240"></iframe>
 </div>
-
-<style type="text/css">
-   
-    #gallery {
-/*        margin: 0 auto;
-        height: 200px   ;
-        width: 66%;
-        margin-left: auto;
-        margin-right: auto;
-        overflow-y: visible;
-        filter: progid:DXImageTransform.Microsoft.Shadow(color='#272229', Direction=135, Strength=10);     */
-        /*height: 520px;*/
-        margin: 0 auto;
-        /*overflow-y: scroll;*/
-        overflow: hidden;
-        width:76%;
-    }
-
-    
-
-    /* This is the pic to display when the hover action occur over the li that contains the thumbnail  */
-    #gallery ul li .mini{
-        /* Animation with transition in Safari and Chrome */
-        -webkit-transition: all 0.6s ease-in-out;
-        /* Animation with transition in Firefox (No supported Yet) */
-        -moz-transition: all 0.6s ease-in-out;
-        /* Animation with transition in Opera (No supported Yet)*/
-        -o-transition: all 0.6s ease-in-out;
-        /* The the opacity to 0 to create the fadeOut effect*/
-        border:1px solid black;
-        /* box shadow effect in Safari and Chrome*/
-        -webkit-box-shadow:#272229 2px 2px 10px;
-        /* box shadow effect in Firefox*/
-        -moz-box-shadow:#272229 2px 2px 10px;
-        /* box shadow effect in IE*/
-        /*filter:progid:DXImageTransform.Microsoft.Shadow(color='#272229', Direction=135, Strength=5); */
-        /* box shadow effect in Browsers that support it, Opera 10.5 pre-alpha release*/
-        box-shadow:#272229 2px 2px 10px;
-    }
-
-
-    #gallery ul li .mini:hover{
-        cursor:pointer;
-    }
-
-
-    /* This create the desired effect of showing the imagen when we mouseover the thumbnail*/
-    #gallery ul li:hover .pic {
-        /* width and height is how much the picture is going to growth with the effect */
-        width:200px; 
-        height:200px;
-        opacity:1; 
-        visibility:visible; 
-        float:right;
-    }
-    #preview{
-        position:absolute;
-        z-index:999;
-        width:50px;
-        height:50px;
-        border:1px solid #ccc;
-        background:#333;
-        padding:5px;
-        display:none;
-        color:#fff;
-        border-radius:8px;
-        width: 310px;
-        height: 174px;
-    } 
-    #preview img{  
-        width: 200px;
-        height: 200px;
-    }
-
-    .loading {
-        width: 100%;
-        height: 100%;
-        position: absolute;
-        top: 30%;
-        left: 45%;
-    }
-    #midHomeImg {
-        width: 100%;
-        position: absolute;
-        z-index: -1;
-        top: 1000px;
-    }
-</style>
-
-
 
 
 <div id="homefboxwidget"  style="width:55%; height:180px; float: right;">
@@ -134,7 +45,7 @@ get_header('ff');
         <script type="text/javascript">
             var m_HomeBoxTotal = <?=file_get_contents("$urlHomeBox?index=-1");?>;
             var m_HomeBoxIndex = 0;
-            console.log('fdlskaf ldkasjhf kldjash fkldjh fkldjah ')
+            console.log(m_HomeBoxTotal);
             $( document ).ready(function() {
                 //getBoxHtml(urlFreeClips + "?index=0",idFreeClips);
                 //getBoxHtml(urlHomeBox + "?index=0",idHomeBox); 
@@ -172,4 +83,94 @@ get_header('ff');
 
 
 <?php get_footer('ff'); ?>
+<!---->
+<!---->
+<!--<style type="text/css">-->
+<!---->
+<!--    #gallery {-->
+<!--        /*        margin: 0 auto;-->
+<!--                height: 200px   ;-->
+<!--                width: 66%;-->
+<!--                margin-left: auto;-->
+<!--                margin-right: auto;-->
+<!--                overflow-y: visible;-->
+<!--                filter: progid:DXImageTransform.Microsoft.Shadow(color='#272229', Direction=135, Strength=10);     */-->
+<!--        /*height: 520px;*/-->
+<!--        margin: 0 auto;-->
+<!--        /*overflow-y: scroll;*/-->
+<!--        overflow: hidden;-->
+<!--        width:76%;-->
+<!--    }-->
+<!---->
+<!---->
+<!---->
+<!--    /* This is the pic to display when the hover action occur over the li that contains the thumbnail  */-->
+<!--    #gallery ul li .mini{-->
+<!--        /* Animation with transition in Safari and Chrome */-->
+<!--        -webkit-transition: all 0.6s ease-in-out;-->
+<!--        /* Animation with transition in Firefox (No supported Yet) */-->
+<!--        -moz-transition: all 0.6s ease-in-out;-->
+<!--        /* Animation with transition in Opera (No supported Yet)*/-->
+<!--        -o-transition: all 0.6s ease-in-out;-->
+<!--        /* The the opacity to 0 to create the fadeOut effect*/-->
+<!--        border:1px solid black;-->
+<!--        /* box shadow effect in Safari and Chrome*/-->
+<!--        -webkit-box-shadow:#272229 2px 2px 10px;-->
+<!--        /* box shadow effect in Firefox*/-->
+<!--        -moz-box-shadow:#272229 2px 2px 10px;-->
+<!--        /* box shadow effect in IE*/-->
+<!--        /*filter:progid:DXImageTransform.Microsoft.Shadow(color='#272229', Direction=135, Strength=5); */-->
+<!--        /* box shadow effect in Browsers that support it, Opera 10.5 pre-alpha release*/-->
+<!--        box-shadow:#272229 2px 2px 10px;-->
+<!--    }-->
+<!---->
+<!---->
+<!--    #gallery ul li .mini:hover{-->
+<!--        cursor:pointer;-->
+<!--    }-->
+<!---->
+<!---->
+<!--    /* This create the desired effect of showing the imagen when we mouseover the thumbnail*/-->
+<!--    #gallery ul li:hover .pic {-->
+<!--        /* width and height is how much the picture is going to growth with the effect */-->
+<!--        width:200px;-->
+<!--        height:200px;-->
+<!--        opacity:1;-->
+<!--        visibility:visible;-->
+<!--        float:right;-->
+<!--    }-->
+<!--    #preview{-->
+<!--        position:absolute;-->
+<!--        z-index:999;-->
+<!--        width:50px;-->
+<!--        height:50px;-->
+<!--        border:1px solid #ccc;-->
+<!--        background:#333;-->
+<!--        padding:5px;-->
+<!--        display:none;-->
+<!--        color:#fff;-->
+<!--        border-radius:8px;-->
+<!--        width: 310px;-->
+<!--        height: 174px;-->
+<!--    }-->
+<!--    #preview img{-->
+<!--        width: 200px;-->
+<!--        height: 200px;-->
+<!--    }-->
+<!---->
+<!--    .loading {-->
+<!--        width: 100%;-->
+<!--        height: 100%;-->
+<!--        position: absolute;-->
+<!--        top: 30%;-->
+<!--        left: 45%;-->
+<!--    }-->
+<!--    #midHomeImg {-->
+<!--        width: 100%;-->
+<!--        position: absolute;-->
+<!--        z-index: -1;-->
+<!--        top: 1000px;-->
+<!--    }-->
+<!--</style>-->
+<!---->
 
--- wp-content/themes/fftheme/header-ff.php
+++ wp-content/themes/fftheme/header-ff.php
@@ -1,11 +1,9 @@
 <!doctype html>
 <html>
-	<head >
+	<head>
         <meta name="description" content="Search Wisely Browse Widely - Footage Search Engine with Free clips from around the web / Royalty free footage and Video backgrounds">
         <meta name="keywords" content="Footage, video footage, free video footage, stock footage, royalty free, hd, vj, timelapse, 4k footage, free 4k footage, video, background, PowerPoint background, Keynote background, presentation background, motion background, loop, motion graphics ,visuals, background visuals, video search, footage search engine ">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <!-- https://developers.facebook.com/tools/debug/og/object/ -->
-        <meta property="og:title" content="Finding Footage" /> 
+        <meta property="og:title" content="Finding Footage" />
         <meta property="og:image" content="http://www.findingfootage.com/videogridengine/images/logo.png" /> 
         <meta property="og:description" content="gives you fast and easy access to multiple video stocks at once, and expand your results. join us to find dozens of free clips online..." /> 
     
@@ -22,15 +20,15 @@
         <script type="text/javascript" src="/wp-content/themes/fftheme/js/main.js"></script>
 
         <?php
-        
-        wp_head(); 
+
+        wp_head();
        
         ?>
 
 	</head>
     
     <body class="ffbody">
-        
+
         <div id="mask">
             <p>
             <img width="100px" height="100px" src="<?php bloginfo( "template_url" ); ?>/images/loader.gif">
@@ -38,7 +36,6 @@
         </div>
             
         <div class="selector"></div>
-
         <header><!-- Start header -->
             <div class="header-wrap">
                 <nav> <!-- Navigation  Convert this into Wordpress paging -->
@@ -62,22 +59,13 @@
                         </div>
                     </div>
                 </nav>
-                <div class="logo-area">
-                    <div id="mainWrapper">
-                        <div id="logoImage"></div>
-                        <div id="bigImage"></div>
-                    </div>
-                    <div class="logo" style="padding-right:3%">
-                        <a href="<?php echo home_url( '/index.php' ); ?>"><img src="<?php bloginfo( 'template_url' ); ?>/images/new/DesktopSlicing/logo.png" alt="logo" width="308" height="172" /></a>
-                    </div>
-                </div>
+               
             </div>
             <?php // do_action( 'bp_header' ) ?>
         </header> <!-- End Of header -->
-    <div class="clear"></div>
 
 
-        <div class="search">
+        <div class="FFsearch">
             <h1>Search for Stock Video</h1>
             <h4>Find Royalty Free footage from multiple websites</h4>
             <input id="searchItem" type="text" onBlur="this.value==''?this.value='Find Footage':''" onClick="this.value=='Find Footage'?this.value='':''" value="Find Footage" class="input-box"  onkeypress="return searchVideo(event)" title="Find Footage" />
@@ -86,58 +74,6 @@
             </div>
         </div>
 
+    <div class="clear"></div>
         <!-- End of Navigation -->
          <?php do_action( 'bp_before_container' ) ?>
-         
- 
-<div id="moreTexts" style="display: none;">   
-        <p> 
-            <span style="text-decoration: underline;">Finding Footage</span>
-    gives you fast and easy access to multiple video stocks at once, and expand your results.
-    Join us to find dozens of free clips online...
-
-</div>
-
-<!-- Google Code for Home page visits Conversion Page -->
-<script type="text/javascript">
-/* <![CDATA[ */
-var google_conversion_id = 880493337;
-var google_conversion_language = "en";
-var google_conversion_format = "3";
-var google_conversion_color = "ffffff";
-var google_conversion_label = "OAABCOiIlGcQmYbtowM";
-var google_remarketing_only = false;
-/* ]]> */
-</script>
-<script type="text/javascript" src="//www.googleadservices.com/pagead/conversion.js">
-</script>
-<noscript>
-<div style="display:inline;">
-<img height="1" width="1" style="border-style:none;" alt="" src="//www.googleadservices.com/pagead/conversion/880493337/?label=OAABCOiIlGcQmYbtowM&amp;guid=ON&amp;script=0"/>
-</div>
-</noscript>
-    <script type="text/javascript">
-
-        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-                (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-            m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-        })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
-        ga('create', 'UA-57796105-1', 'auto');
-        ga('send', 'pageview');
-
-</script>
-<script src='https://www.google.com/recaptcha/api.js?hl=en'></script>
-<!-- Facebook Pixel Code -->
-<script>
-    !function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-        n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;
-        n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;
-        t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window,
-        document,'script','https://connect.facebook.net/en_US/fbevents.js');
-
-    fbq('init', '605851989567219');
-    fbq('track', "PageView");</script>
-<noscript><img height="1" width="1" style="display:none"
-               src="https://www.facebook.com/tr?id=605851989567219&ev=PageView&noscript=1"/></noscript>
-    <!-- End Facebook Pixel Code -->
